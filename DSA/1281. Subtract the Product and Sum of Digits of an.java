fd3944d9afa0bb9081babcd3f4472bfbce2b65a4
namita7448 <79524364+namita7448@users.noreply.github.com>
2022-12-08 23:56:31 +0530

    Create 1281. Subtract the Product and Sum of Digits of an.java

---
+++ DSA/1281. Subtract the Product and Sum of Digits of an.java
@@ -0,0 +1,20 @@
+class Solution {
+    public static int subtractProductAndSum(int n) {
+        int rem;
+        int prod = 1;
+        int sum = 0;
+        while(n>0){
+            rem = n%10;
+            prod = rem * prod;
+            sum  = sum + rem;
+            n = n/10;
+        }
+        int result = (prod - sum);
+        return result;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(subtractProductAndSum(n));
+    } 
+}
