f83f86cb421fb853a28f04b3e213a4fe4af4a11f
namita7448 <79524364+namita7448@users.noreply.github.com>
2022-12-17 22:51:02 +0530

    Create 1389. Create Target Array in the Given Order.java

---
+++ DSA/1389. Create Target Array in the Given Order.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] createTargetArray(int[] nums, int[] index) {
+        List<Integer> list = new ArrayList<Integer>();
+        int n = nums.length;
+        int[] arr = new int[n];
+        for(int i = 0; i<n; i++){
+            list.add(index[i],nums[i]);
+        }
+        for(int i = 0; i<n; i++){
+            arr[i] = list.get(i);
+        }
+        return arr;
+    }
+}
