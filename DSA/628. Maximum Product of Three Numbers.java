4608efb19f076ec63b11b635f4b9f6fbb21c437e
namita7448 <79524364+namita7448@users.noreply.github.com>
2023-01-17 23:49:34 +0530

    Create 628. Maximum Product of Three Numbers.java

---
+++ DSA/628. Maximum Product of Three Numbers.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int max = Integer.MIN_VALUE;
+        int res = 0;
+        for(int i = 0; i < n-2; i++){
+            int j = i+1, k = n-1;
+            while(j < k){
+                res = nums[i]*nums[j]*nums[k];
+                if(max < res){
+                    max = res;
+                    j++;
+                }else{
+                    break;
+                }
+            }
+        }
+        return max;
+    }
+}
