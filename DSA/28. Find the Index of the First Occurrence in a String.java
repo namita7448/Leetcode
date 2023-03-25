54733cd65ca77a9937a3767f8d07709eea4d26e3
namita7448 <79524364+namita7448@users.noreply.github.com>
2023-03-26 00:04:03 +0530

    Update 28. Find the Index of the First Occurrence in a String.java

--- DSA/28. Find the Index of the First Occurrence in a String.java
+++ DSA/28. Find the Index of the First Occurrence in a String.java
@@ -7,5 +7,20 @@ class Solution {
         else{
             return -1;
         }
+        //without using built-in function
+        int len1 = haystack.length();
+        int len2 = needle.length();
+        for(int  i = 0; i <= len1-len2; i++){
+            int j;
+            for(j = 0; j < len2; j++){
+                if(haystack.charAt(i + j) != needle.charAt(j)){
+                    break;
+                }
+            }
+            if(j == len2){
+                return i;
+            }
+        }
+        return -1;
     }
 }
